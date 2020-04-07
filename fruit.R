319a321817c1c3fe3b150e52099644a916d1c04e
dks1013 <59687167+dks1013@users.noreply.github.com>
2020-04-07 11:17:35 +0900

    fruit 작성
    과제를 위한 fruit작성한 내용

---
+++ fruit.R
@@ -0,0 +1,12 @@
+product <- c("사과", "딸기", "수박")
+price <- c(1800, 1500, 3000)
+salecnt <- c(24, 38, 13)
+
+fruit <- data.frame(product, price, salecnt)
+fruit
+## fruit 데이터 프레임 만든 코드 
+
+fruit[1,]
+## 사과 행 데이터만 뽑아 출력한 코드 
+
+

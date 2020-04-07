c361ceaf4620b2cac1eaa5d2b84431d3fb9d0f7b
dks1013 <59687167+dks1013@users.noreply.github.com>
2020-04-07 13:06:28 +0900

    데이터 불러오기 까지

--- fruit.R
+++ fruit.R
@@ -29,3 +29,12 @@ detach(fruit)
 fruit
 ## totprice 파생변수(price*salecnt) 만든 코드 
 
+write.csv(fruit, file = "C:/data/fruit.csv", quote = FALSE)
+##fruit데이터프레임을 csv파일로 저장하기 
+
+install.packages("readxl")
+library(readxl)
+air_df <- data.frame(read_xlsx("C:/data/air.xlsx"))
+air_df
+## air 엑셀파일 불러와서 데이터 프레임 만들기
+

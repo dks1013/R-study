03e1ac9817caa87d13f562933ab6ce6dab2ffc62
dks1013 <59687167+dks1013@users.noreply.github.com>
2020-04-07 12:25:31 +0900

    파생 변수까지의 코드
    중간 과제 저장

--- fruit.R
+++ fruit.R
@@ -3,7 +3,7 @@ price <- c(1800, 1500, 3000)
 salecnt <- c(24, 38, 13)
 
 fruit <- data.frame(product, price, salecnt)
-fruit
+str(fruit)
 ## fruit 데이터 프레임 만든 코드 
 
 fruit[1,]
@@ -12,5 +12,20 @@ fruit[1,]
 fruit[c(1,3)]
 ## product와 salecnt 한번에 출력 코드
 
+sapply(c(list(fruit$price),list(fruit$salecnt)), mean)
+## price와 salecnt의 평균 출력 코드 
 
+product <- c("사과", "딸기", "수박")
+price <- c(1800, 1500, 3000)
+salecnt <- c(24, 38, 13)
+
+fruit <- data.frame(product, price, salecnt, stringsAsFactors = FALSE)
+str(fruit)
+## 데이터 프레임 문자열 살리는 코드
+
+attach(fruit)
+fruit$totprice <- price*salecnt
+detach(fruit)
+fruit
+## totprice 파생변수(price*salecnt) 만든 코드 
 
